7b094a5bdfbda0069dc2d8c4cc1b660988a11e1c
yagnishev <yagnyshevgs@gmail.com>
2022-08-19 12:30:20 +0300

    Initial commit

---
+++ src/test/java/com/example/springopenapirest/SpringOpenapiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springopenapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenapiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
